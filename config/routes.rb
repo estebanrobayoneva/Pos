7f02357d126b4b7bb081a0b23b467c35c3f6be9f
Leonardo merchan <leonardo.merchan.1992@gmail.com>
2017-03-17 17:22:23 +0000

    editar el registro del usuario

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,7 @@ Rails.application.routes.draw do
   resources :clients
   resources :categories
   resources :products
-  devise_for :users
+  devise_for :users, :controllers => { :registrations => "user/registrations" }
     root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
